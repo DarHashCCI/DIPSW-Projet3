7db51c789c0a99741a89cb560153786ca8381515
DarHashCCI <alss-dipsw20-hda@ccicampus.fr>
2021-03-27 22:55:10 +0100

    added a dashboard with only the last 3 contacts added.

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class UsersController extends AbstractController
 {
 
     public function index(): Response
     {
+        $contain=[];
+        $repository = $this->getDoctrine()->getRepository(Contact::class);
+        $conts=$repository->showLastThreeContacts();
+        //$conts=$repository->findAll();
+        foreach($conts as $cont){
+            $contain[$cont->getId()]=json_decode($cont->getInfos(),true);
+        }
+        //dd($contain);
         return $this->render('users/index.html.twig', [
-            'controller_name' => 'UsersController',
+            'request' => $contain,
         ]);
     }
 }
