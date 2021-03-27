584acf4044bdb31775d20609c74b02df9caac494
DarHashCCI <alss-dipsw20-hda@ccicampus.fr>
2021-03-27 22:29:30 +0100

    proper redirection once logged added

--- src/Security/LoginAuthentificatorAuthenticator.php
+++ src/Security/LoginAuthentificatorAuthenticator.php
@@ -97,9 +97,9 @@ class LoginAuthentificatorAuthenticator extends AbstractFormLoginAuthenticator i
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
-
+        return new RedirectResponse($this->urlGenerator->generate('users_home'));
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
