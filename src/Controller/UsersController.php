deadaaf2f39e36def381d3c0fc3c5cd56fe2c703
DarHashCCI <alss-dipsw20-hda@ccicampus.fr>
2021-03-28 16:17:12 +0200

    Started working on the session related redirections

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -6,27 +6,54 @@ use App\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class UsersController extends AbstractController
 {
-
-    public function index(): Response
+    //Redirection index
+    //If user logged, redirected straight to dashboard
+    //Else, sent to login window
+    public function index():Response
     {
-        $contain=[];
-        $repository = $this->getDoctrine()->getRepository(Contact::class);
-        $conts=$repository->showLastThreeContacts();
-        //$conts=$repository->findAll();
-        foreach($conts as $cont){
-            $contain[$cont->getId()]=json_decode($cont->getInfos(),true);
+        $session = new Session();
+        if($session->get('_security.last_username')==null){
+            return $this->redirectToRoute('app_login');
         }
-        //dd($contain);
-        return $this->render('users/index.html.twig', [
-            'request' => $contain,
-        ]);
+        else return $this->redirectToRoute('users_home');
     }
 
-    public function profile (Request $request)
+    //User dashboard
+    public function home(): Response
     {
+        $session = new Session();
+        if($session->get('_security.last_username')==null){
+            return $this->redirectToRoute('app_login');
+        }
+        else{
+            $contain=[];
+            $repository = $this->getDoctrine()->getRepository(Contact::class);
+            $conts=$repository->showLastThreeContacts();
+            //$conts=$repository->findAll();
+            foreach($conts as $cont){
+                $contain[$cont->getId()]=json_decode($cont->getInfos(),true);
+            }
+            //dd($contain);
+            return $this->render('users/index.html.twig', [
+                'request' => $contain,
+            ]);
+        }
+    }
 
+    public function profile (Request $request,$id)
+    {
+        $session = new Session();
+        if($session->get('_security.last_username')==null){
+            return $this->redirectToRoute('app_login');
+        }
+        else{
+            return $this->render('users/profile.html.twig', [
+                'id' => $id,
+            ]);
+        }
     }
 }
