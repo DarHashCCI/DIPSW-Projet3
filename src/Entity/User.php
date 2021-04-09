08ec778dd7f47ed3fdf76a62cafe1bf5f345df9d
DarHashCCI <alss-dipsw20-hda@ccicampus.fr>
2021-04-09 22:13:46 +0200

    Register and registration successful: frontend done. Just need the logo.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="Un email existe déjà avec ce compte.")
  */
 class User implements UserInterface
 {
